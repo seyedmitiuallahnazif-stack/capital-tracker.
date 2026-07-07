edb886d7cd1167c47b65d8e475a3c4367f3d0305
seyedmitiuallahnazif-stack <seyed.mitiuallah.nazif@gmail.com>
2026-07-07 23:53:50 +0430

    Update cache version to 'capital-tracker-v2'

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'capital-tracker-v1';
+const CACHE = 'capital-tracker-v2';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
